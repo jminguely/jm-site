91d4b41b859b6338a3732293362fa90d77e194aa
Julien Minguely <jminguely@gmail.com>
2023-01-18 08:55:33 +0100

    Adds gitignore

--- dist/index.2241e1bf.js
+++
@@ -1,2 +0,0 @@
-const init=function(){document.getElementById("menuToggle").addEventListener("click",(function(){document.body.classList.toggle("menuOpen")}))};document.addEventListener("DOMContentLoaded",(function(){document.getElementById("menuToggle").addEventListener("click",(function(){document.body.classList.toggle("menuOpen")}))}));
-//# sourceMappingURL=index.2241e1bf.js.map
